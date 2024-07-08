a901817137ae51ac1283ae694a6ce55ce9803038
KDY <lemon8818@naver.com>
2024-07-08 17:41:23 +0900

    20240708

---
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,53 @@
+package polymorphism;
+
+public class LgTV implements TV{
+
+	//ctrl+space 기본생성자
+	public LgTV() {
+		System.out.println("========> LgTV 객체 생성");
+	}
+	
+	@Override
+	public void powerOn() {
+		System.out.println("LgTV---전원 켠다.");
+		
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("LgTV---전원 끈다.");
+		
+	}
+
+	@Override
+	public void volumeUp() {
+		System.out.println("LgTV---소리 올린다.");
+		
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("LgTV---소리 내린다.");
+		
+	}
+
+	/*
+	public void turnOn() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("LgTV---전원 끈다.");
+	}
+	
+	public void soundUp() {
+		System.out.println("LgTV---소리 올린다.");
+	}
+	
+	public void soundDown() {
+		System.out.println("LgTV---소리 내린다.");
+	}
+	*/
+	
+	
+}
---
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -0,0 +1,61 @@
+package polymorphism;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext factory =
+				new GenericXmlApplicationContext("applicationContext.xml");
+		
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+		
+		tv1.powerOn();
+		tv1.volumeUp();
+		tv1.volumeDown();
+		tv1.powerOff();
+	
+		tv2.powerOn();
+		tv2.volumeUp();
+		tv2.volumeDown();
+		tv2.powerOff();
+		
+		tv3.powerOn();
+		tv3.volumeUp();
+		tv3.volumeDown();
+		tv3.powerOff();
+		
+		factory.close();
+		
+		
+		/*
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV)factory.getBean(args[0]);
+		
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		*/
+		
+		/*
+		TV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		*/
+		
+		/*
+		SamsungTV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		*/
+	}
+}
