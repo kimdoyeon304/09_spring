13f40b0e19926ff16a122b104662c96efa7c05dd
KDY <lemon8818@naver.com>
2024-07-17 17:10:22 +0900

    20240717 Mybatis프레임워크 및 스프링과 연동 후 게시판실습

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -14,10 +14,11 @@ import com.springbook.biz.common.LogAdvice;
 public class BoardServiceImpl implements BoardService{
 
 	@Autowired
-	private BoardDAO boardDAO;
+	//private BoardDAO boardDAO;
 	//private BoardDAOSpring boardDAO;
 	//private LogAdvice log;
 	//private Log4jAdvice log;
+	private BoardDAOMybatis boardDAO;
 	
 //	public BoardServiceImpl() {
 //		log = new LogAdvice();
