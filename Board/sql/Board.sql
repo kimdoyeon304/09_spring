75a12e84c8a8a212574d5063ad7093bcbb6c4d2e
KDY <lemon8818@naver.com>
2024-07-17 17:09:46 +0900

    20240717 Mybatis프레임워크 및 스프링과 연동 후 게시판실습

---
+++ Board/sql/Board.sql
@@ -0,0 +1,15 @@
+create table lbi_board(
+bno number generated always as IDENTITY,
+title varchar2(150) not null,
+content varchar2(2000) not null,
+writer varchar2(50) not null,
+regdate date default sysdate,
+updatedate date default sysdate,
+CONSTRAINT pk_board PRIMARY KEY(bno)
+);
+
+insert into lbi_board(title, content, writer) values ('테스트 제목', '테스트 내용', '작가');
+
+commit;
+
+select * from lbi_board;
